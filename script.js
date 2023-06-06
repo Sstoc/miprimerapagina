2f250f71bd045696ffae0f3507702654df654309
Andrés Moreno <aandres.moreno3@gmail.com>
2023-06-06 19:01:14 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function showAlert() { alert('hola esto es una alerta desde javascript');}
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
+                      
